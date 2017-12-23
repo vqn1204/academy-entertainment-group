8b2253ad5102cbff85b853383e9d1420e561421e
Vincent Nguyen <vqn@Vincents-MacBook-Pro.local>
2017-12-22 21:41:16 -0500

    added first member

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
+
     <div id="about">
       <b-img src="https://i.imgur.com/9OlyQPH.jpg" fluid alt="Fluid Image"/>
       <b-jumbotron header="About">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec faucibus massa. Pellentesque vitae imperdiet ex. Quisque posuere porttitor elit nec faucibus. Suspendisse potenti. Nullam odio nisi, suscipit in interdum nec, sodales a est. Nam aliquam sem vel libero scelerisque pulvinar. Ut ac nisl nec leo dapibus venenatis. Donec nunc purus, dictum sed lectus eget, porta pretium erat. Cras accumsan fermentum tortor at luctus. Nulla facilisi. Curabitur pretium aliquet tellus, quis euismod justo sagittis in. Vivamus vel ex auctor, rhoncus sem vitae, convallis nunc. Maecenas vitae odio tincidunt, faucibus tortor eget, maximus lectus. Fusce id ex ante. </p>
+        <p id="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec faucibus massa. Pellentesque vitae imperdiet ex. Quisque posuere porttitor elit nec faucibus. Suspendisse potenti. Nullam odio nisi, suscipit in interdum nec, sodales a est. Nam aliquam sem vel libero scelerisque pulvinar. Ut ac nisl nec leo dapibus venenatis. Donec nunc purus, dictum sed lectus eget, porta pretium erat. Cras accumsan fermentum tortor at luctus. Nulla facilisi. Curabitur pretium aliquet tellus, quis euismod justo sagittis in. Vivamus vel ex auctor, rhoncus sem vitae, convallis nunc. Maecenas vitae odio tincidunt, faucibus tortor eget, maximus lectus. Fusce id ex ante. </p>
       </b-jumbotron>
+      <b-img src="https://i.imgur.com/V7utNF9.jpg" fluid alt="Fluid Image"/>
     </div>
+
+
+    <div id="members">
+      <h1>Members</h1>
+        <b-container fluid>
+          <b-row align-v="center" no-gutters>
+            <b-col no-gutters>
+              <b-img class="thumb" rounded="circle" thumbnail src="https://i.imgur.com/GFoj0nv.jpg" alt="Thumbnail"/>
+              <h3>Pierce Ripanti</h3>
+              <a target="_blank" href="http://www.instagram.com/primetimepierce"><img src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram30x30.png"></a>
+              <a target="_blank" href="http://www.facebook.com/pierce.ripanti"><img src="//login.create.net/images/icons/user/facebook_30x30.png"></a>
+            </b-col>
+            <b-col no-gutters>
+        <p id="member-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec faucibus massa. Pellentesque vitae imperdiet ex. Quisque posuere porttitor elit nec faucibus. Suspendisse potenti. Nullam odio nisi, suscipit in interdum nec, sodales a est. Nam aliquam sem vel libero scelerisque pulvinar. Ut ac nisl nec leo dapibus venenatis. Donec nunc purus, dictum sed lectus eget, porta pretium erat. Cras accumsan fermentum tortor at luctus. Nulla facilisi. Curabitur pretium aliquet tellus, quis euismod justo sagittis in. Vivamus vel ex auctor, rhoncus sem vitae, convallis nunc. Maecenas vitae odio tincidunt, faucibus tortor eget, maximus lectus. Fusce id ex ante. </p>
+            </b-col>
+          </b-row>
+        </b-container>
+    </div>
+
   </div>
 </template>
 
@@ -17,14 +38,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  p {
-    padding: 0em 1em 1em 1em;
-    text-align: justify;
-  }
+#about-text {
+  padding: 0em 1em 1em 1em;
+  text-align: justify;
+}
 
-  .jumbotron {
-    background-color: black;
-    color: white;
-    padding: 2rem 2rem;
-  }
+.jumbotron {
+  background-color: black;
+  color: white;
+  padding: 2rem 2rem;
+  margin-bottom: 0;
+}
+
+h1 {
+  font-size: 4.5rem;
+  font-weight: 300;
+  line-height: 1.2;
+  padding-top: 0.5em;
+}
+
+#members {
+  background-color: black;
+  color: white;
+}
+
+.rounded-circle {
+  padding: 0.1rem;
+  background-color: black;
+}
+
+.thumb {
+  height: auto;
+  width: auto;
+  max-width: 75%;
+  max-height: 75%;
+}
+
+#member-text {
+  margin-right: 1.5em;
+  text-align: justify;
+}
 </style>
